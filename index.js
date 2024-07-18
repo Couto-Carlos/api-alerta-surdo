086b2334b996baf5930906d795f012aaf22907f1
Couto-Carlos <caduco2003@gmail.com>
2024-07-18 20:02:16 -0300

    Create project

---
+++ index.js
@@ -0,0 +1,32 @@
+const { log } = require('console')
+const { Socket } = require('socket.io')
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173', }})
+
+const PORT = process.env.PORT || 3001
+
+io.on('connection', socket => {
+    console.log('Usuário conectado!', socket.id)
+
+    socket.on('disconnect', reason => {
+        console.log('Usuário desconectado!', socket.id)
+    })
+
+    socket.on('set_username', username => {
+        socket.data.username = username
+        console.log(socket.data.username)
+    })
+
+    socket.on('message', ({ text, location }) => {
+        io.emit('receive_message', {
+            text,
+            authorId: socket.id,
+            author: socket.data.username,
+            location
+        });
+    });
+})
+
+server.listen(PORT,() =>  console.log('Server runing...'))
