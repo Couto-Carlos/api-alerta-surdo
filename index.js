bb1f1774319cbb79e4a6bf9dc33d255ef30c00cd
Couto-Carlos <caduco2003@gmail.com>
2024-07-19 09:24:32 -0300

    Change index.js

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const { log } = require('console')
-const { Socket } = require('socket.io')
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const app = require('express')()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: { 
-    origin: '*', // Permitir qualquer origem durante o desenvolvimento
-    methods: ["GET", "POST"]
-} })
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 io.on('connection', socket => {
-    console.log('Usuário conectado!', socket.id)
+  console.log('Usuário conectado!', socket.id);
 
-    socket.on('disconnect', reason => {
-        console.log('Usuário desconectado!', socket.id)
-    })
+  socket.on('disconnect', reason => {
+    console.log('Usuário desconectado!', socket.id);
+  });
 
-    socket.on('set_username', username => {
-        socket.data.username = username
-        console.log(socket.data.username)
-    })
+  socket.on('set_username', username => {
+    socket.data.username = username;
+    console.log(socket.data.username);
+  });
 
-    socket.on('message', ({ text, location }) => {
-        io.emit('receive_message', {
-            text,
-            authorId: socket.id,
-            author: socket.data.username,
-            location
-        });
+  socket.on('message', ({ text, location }) => {
+    io.emit('receive_message', {
+      text,
+      authorId: socket.id,
+      author: socket.data.username,
+      location
     });
-})
+  });
+});
 
-server.listen(PORT,() =>  console.log('Server runing...'))
+server.listen(PORT, () => console.log('Server running...'));
